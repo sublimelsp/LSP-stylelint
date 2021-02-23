aa7fa447e21ac565cabef0f39a5b8c46b7458cad
Rafał Chłodnicki <rchl2k@gmail.com>
2021-02-24 00:05:35 +0100

    Fix: Use ${node_bin} for the node binary
    With the latest version of lsp_utils a change was introduced [1] that allows using a locally
    managed node runtime instead of the system one. For that to work, the "node" command
    needs to use a variable.
    
    [1] https://github.com/sublimelsp/lsp_utils/commit/403345a0c5c15e84802c712044c630a9fb236b9d

--- plugin.py
+++ plugin.py
@@ -14,7 +14,3 @@ class LspStylelintPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'server.js')
-
-    @classmethod
-    def install_in_cache(cls) -> bool:
-        return False
