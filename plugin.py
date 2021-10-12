065e88007fb5da7ade52b539ac41bfb4b5d263d0
Rafal Chlodnicki <rchl2k@gmail.com>
2021-10-12 08:58:15 +0200

    Set minimum node version to 14

--- plugin.py
+++ plugin.py
@@ -1,5 +1,6 @@
-import os
+from LSP.plugin.core.typing import Tuple
 from lsp_utils import NpmClientHandler
+import os
 
 
 def plugin_loaded():
@@ -15,3 +16,7 @@ class LspStylelintPlugin(NpmClientHandler):
     server_directory = 'language-server'
     server_binary_path = os.path.join(server_directory, 'dist', 'server.js')
     skip_npm_install = True
+
+    @classmethod
+    def minimum_node_version(cls) -> Tuple[int, int, int]:
+        return (14, 0, 0)
