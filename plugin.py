c5876cf1368d2b6d27a1d1f156b380c365e3297c
Предраг Николић <idmpepe@gmail.com>
2022-02-26 21:19:51 +0100

    update server to 1.2.2

--- plugin.py
+++ plugin.py
@@ -14,7 +14,7 @@ def plugin_unloaded():
 class LspStylelintPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = 'language-server'
-    server_binary_path = os.path.join(server_directory, 'dist', 'server.js')
+    server_binary_path = os.path.join(server_directory, 'dist', 'start-server.js')
     skip_npm_install = True
 
     @classmethod
